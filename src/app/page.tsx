79f8d317c1a62f37f33ea8ddd785af2dd747a55d
Julia Subal <67679281+js-geo@users.noreply.github.com>
2024-07-20 23:45:54 +0200

    clock icon & refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col gap-20 justify-center items-center bg-[#3C50E1]">
-      <Image src="/logo.png" alt="scholz with crown" width={400} height={400} />
+      <div className="relative w-[25rem] h-[12rem]">
+        <Image src="/logo.png" alt="crowns and clowns logo" fill />
+      </div>
       <Link
         className="text-white py-[0.625rem] px-[4.25rem] bg-[#3C50E1] rounded-[3rem] border border-white hover:bg-[#2c3aa6]"
         href="/game/question/0"
--- src/components/feedback/feedback.tsx
+++ src/components/feedback/feedback.tsx
@@ -16,7 +16,7 @@ export const Feedback: React.FC<
       open={open}
       className="bg-white h-[calc(100vh-60px)] w-screen fixed top-[60px]"
     >
-      <div className="flex flex-col gap-4 h-full justify-center items-center px-6">
+      <div className="flex flex-col gap-8 h-full justify-center items-center px-6 mt-8">
         <span className="font-bold text-4xl text-center">
           {isCorrect ? (
             <>Du bist Champion der Demokratie!</>
@@ -38,10 +38,34 @@ export const Feedback: React.FC<
             height={200}
           />
         )}
-        <div className="text-center">
+        <div className="text-left border-2 border-[#3AB795] drop-shadow-md p-6 pt-12 rounded-xl bg-white">
+          <div className="absolute -top-2 flex flex-row text-[#3AB795]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="50"
+              viewBox="0 0 36 50"
+              fill="none"
+            >
+              <path
+                d="M33 0L34.5 2L36 4H33V0Z"
+                fill="#3AB795"
+                fillOpacity="0.3"
+              />
+              <path
+                d="M0 0H16.5H33V27.1426C33 28.0054 32.6285 28.8265 31.9803 29.3961L18.4803 41.2597C17.3477 42.255 15.6523 42.255 14.5197 41.2597L1.01966 29.3961C0.371517 28.8265 0 28.0054 0 27.1426V0Z"
+                fill="#3AB795"
+              />
+            </svg>
+            <span className="ml-3 mt-4">Lösung</span>
+          </div>
           {children} Die richtige Antwort ist {`"${question?.correctAnswer}"`}.
         </div>
-        <Button variant="primary" className="text-center" onClick={onClick}>
+        <Button
+          variant="primary"
+          className="text-center mt-auto mb-16"
+          onClick={onClick}
+        >
           Nächste Frage
         </Button>
       </div>
