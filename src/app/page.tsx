d8075b5ad172acffb75904fd0eeab3153b6ff0da
Julia Subal <67679281+js-geo@users.noreply.github.com>
2024-07-21 12:21:03 +0200

    update questions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col gap-20 justify-center items-center bg-[#3C50E1]">
       <div className="relative w-[25rem] h-[12rem]">
-        <Image src="/logo.png" alt="crowns and clowns logo" fill />
+        <Image
+          src="/logo.png"
+          priority={true}
+          alt="crowns and clowns logo"
+          fill
+        />
       </div>
       <Link
         className="text-white py-[0.625rem] px-[4.25rem] bg-[#3C50E1] rounded-[3rem] border border-white hover:bg-[#2c3aa6]"
